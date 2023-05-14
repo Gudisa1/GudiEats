7583826bfe46b915cdd37b19bb7ebc3cf17a8de5
Gudisa Gebi <gudisagebi1@gmail.com>
2023-05-14 01:47:42 -0700

    Frontend & Tailwind Setup Completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-red-500 bg-gray-200 p-4">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
